74c4b9a17a40a6fc8b93e28f3ecaf211e467c816
OmniaAbdelraheem <tur52752@temple.edu>
2024-09-25 15:41:25 -0400

    Organized the items I added an error message to the user, if the user clicked on the button it'll give them an error message saying "Name is required"

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -8,21 +8,25 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    // Declare view properties - the first one is done for you
-    lateinit var displayTextView: TextView
+    private lateinit var displayTextView: TextView
+    private lateinit var nameEditText: EditText
+    private lateinit var clickMeButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
+        nameEditText = findViewById(R.id.nameEditText)
+        clickMeButton = findViewById(R.id.clickMeButton)
 
-        
-        findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+        clickMeButton.setOnClickListener {
+            val name = nameEditText.text.toString()
+            if (name.isEmpty()) {
+                nameEditText.error = "Name is required!"
+            } else {
+                displayTextView.text = "Hello, $name"
+            }
         }
-
-
     }
 }
